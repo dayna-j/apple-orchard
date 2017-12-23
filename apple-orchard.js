7797495efed6b8dfb7e745ee48fa027b926cc6b0
Dayna Blackwell <daynajblackwell@gmail.com>
2017-12-23 15:41:38 -0700

    Apple correctly inherits from Fruit

--- apple-orchard.js
+++ apple-orchard.js
@@ -1,51 +1,44 @@
 function Fruit() {
 	this.__age = 0;
-	this.fruitMsg = function () {
-		console.log("I am fruit!");
 	}
-	
-	this.getAge = function (){
-		return this.__age;
-	}
-	
-	this.incrAge = function (){
-		this.__age++;
-		console.log("Apple is 1 week older")
-	}
-	
-	this.reSetAge = function (){
-		this.__age = 0;
-		console.log("age reset to 0");
-	}
-// all fruit rots; implement rotting method that checks the age of the fruit
-}
 
 Fruit.prototype.checkRot = function () {
-	if(this.getAge > 3){console.log("this piece of fruit is rotten..")}
-	else {console.log("It's still fresh enough to eat.")}
+	if(this.getAge() > 3){
+		console.log("this piece of fruit is rotten..");
+	}
+	else {
+		console.log("It's still fresh enough to eat.")
+	}
 };
+
+Fruit.prototype.fruitMsg = function () {
+	console.log("I am fruit");
+}
+
+Fruit.prototype.getAge = function () {
+	return this.__age;
+}
+
+Fruit.prototype.IncrAge = function () {
+	this.__age++;
+	console.log("Apple is 1 week older")
+}
+
+Fruit.prototype.reSetAge = function () {
+	this.__age = 0;
+	console.log("age reset to 0");
+	}
+
+
+
 
 
 function Apple(weight, cultivar) {
 	this.weight = weight;
 	this.cultivar = cultivar;
-//	this.__age = 0;
-
-//	this.getAge = function(){
-//		return this.__age;
-//	}
-//	
-//	this.incrAge = function(){
-//		this.__age++;
-//		console.log("Apple is 1 week older")
-//	}
-//	
-//	this.reSetAge = function(){
-//		this.__age = 0;
-//		console.log("age reset to 0");
-//	}
-
 }
+
+Apple.prototype = new Fruit();
 
 function pickApples()
 {
@@ -67,7 +60,6 @@ function pickApples()
 		appleWeight = prompt("Enter weight for Apple "+i+": ");
 		appleCultivar = prompt("Enter cultivar for apple "+i+": ");
 		tempApple = new Apple(appleWeight, appleCultivar);
-		tempApple = Object.setPrototypeOf(tempApple, Fruit);
 		appleBasket[i] = tempApple;
 	}		
 	return appleBasket;
